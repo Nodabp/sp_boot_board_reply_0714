ba599885b3139e899bd1c4e259c3dee4e0f2e162
byeongmin <sa98077@naver.com>
2025-07-08 14:19:58 +0900

    spring boot prac start

---
+++ src/main/java/com/sa98077/sp_boot_board_prac/SpBootBoardPracApplication.java
@@ -0,0 +1,13 @@
+package com.sa98077.sp_boot_board_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpBootBoardPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpBootBoardPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sa98077/sp_boot_board_prac/SpBootBoardPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa98077.sp_boot_board_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpBootBoardPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
